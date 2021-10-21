9b07260f921873e8af7664c95019ae23bece5227
Flame <flameartme@gmail.com>
2021-10-21 15:04:34 +0300

    custom template available in opts now

--- src/components/RESTTable.vue
+++ src/components/RESTTable.vue
@@ -258,33 +258,6 @@ Vue.use(VuejsDatatableFactory);
 
 // Настраиваем дизайн таблицы
 let thatTable = VuejsDatatableFactory.useDefaultType(false)
-    .registerTableType('datatable', tableType => tableType
-        .mergeSettings({
-          table: {
-            class: 'table table-hover table-striped no-border',
-            row: {
-              class: 'datatable-edit-link'
-            },
-
-            sorting: {
-              sortAsc: '<svg width="13px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-up-alt" class="svg-inline--fa fa-sort-amount-up-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M240 96h64a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm0 128h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm256 192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-256-64h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zM16 160h48v304a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V160h48c14.21 0 21.39-17.24 11.31-27.31l-80-96a16 16 0 0 0-22.62 0l-80 96C-5.35 142.74 1.78 160 16 160z"></path></svg>',
-              sortDesc: '<svg width="13px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-down" class="svg-inline--fa fa-sort-amount-down fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M304 416h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-128-64h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352zm256-192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-64 128H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM496 32H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>',
-              sortNone: '<svg width="8px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort" class="svg-inline--fa fa-sort fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path></svg>', //'<i class="fas fa-sort" title="Sort"></i>',
-            },
-
-          },
-          pager: {
-            classes: {
-              pager: 'pagination text-center',
-              li: 'page-item page-link',
-              selected: 'active',
-            },
-            icons: {
-              next: '<i class="fas fa-chevron-right" title="Next page"></i>',
-              previous: '<i class="fas fa-chevron-left" title="Previous page"></i>',
-            },
-          },
-        }));
 
 export default {
   name: "vueresttable",
@@ -305,6 +278,10 @@ export default {
     columnsupdated: Function,
     rowsupdated: Function,
     beforeGetRows: Function
+  },
+  created() {
+    thatTable.registerTableType('datatable', tableType => tableType
+        .mergeSettings(this.optsInfo.template));
   },
   data: function () {
     return {
@@ -1151,7 +1128,33 @@ export default {
         canEdit: true,
         where: {},
         filterRowMargin: 0,
-        popupRowMargin: 10
+        popupRowMargin: 10,
+        template: {
+          table: {
+            class: 'table table-hover table-striped no-border',
+            row: {
+              class: 'datatable-edit-link'
+            },
+
+            sorting: {
+              sortAsc: '<svg width="13px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-up-alt" class="svg-inline--fa fa-sort-amount-up-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M240 96h64a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm0 128h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zm256 192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-256-64h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16zM16 160h48v304a16 16 0 0 0 16 16h32a16 16 0 0 0 16-16V160h48c14.21 0 21.39-17.24 11.31-27.31l-80-96a16 16 0 0 0-22.62 0l-80 96C-5.35 142.74 1.78 160 16 160z"></path></svg>',
+              sortDesc: '<svg width="13px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort-amount-down" class="svg-inline--fa fa-sort-amount-down fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M304 416h-64a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h64a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-128-64h-48V48a16 16 0 0 0-16-16H80a16 16 0 0 0-16 16v304H16c-14.19 0-21.37 17.24-11.29 27.31l80 96a16 16 0 0 0 22.62 0l80-96C197.35 369.26 190.22 352 176 352zm256-192H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm-64 128H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zM496 32H240a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h256a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>',
+              sortNone: '<svg width="8px" style="color: gray" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort" class="svg-inline--fa fa-sort fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path></svg>', //'<i class="fas fa-sort" title="Sort"></i>',
+            },
+
+          },
+          pager: {
+            classes: {
+              pager: 'pagination text-center',
+              li: 'page-item page-link',
+              selected: 'active',
+            },
+            icons: {
+              next: '<i class="fas fa-chevron-right" title="Next page"></i>',
+              previous: '<i class="fas fa-chevron-left" title="Previous page"></i>',
+            },
+          },
+        }
       }, this.opts);
     },
   }
